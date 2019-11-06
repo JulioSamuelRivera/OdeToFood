a43eb280dab6007835e9c5b7a57827b008a2b771
Julio S. Rivera <Jrivera955@gmail.com>
2019-11-05 20:03:17 -0500

    Added db context and ef nugget packages

---
+++ OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+        public void Add(Restaurant restaurant)
+        {
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
